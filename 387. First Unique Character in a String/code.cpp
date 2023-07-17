6be50972e119fa83368557321cdb550d549f369e
Bharat Swami <79807425+bharat1718swami@users.noreply.github.com>
2023-07-18 00:33:02 +0530

    Easy, Stack, Design

---
+++ 387. First Unique Character in a String/code.cpp
@@ -0,0 +1,24 @@
+// 9-6-22
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        for(int i=0;i<s.length();i++){
+            int j=0;
+          //  cout<<s[j]<<endl;
+            for(j=0;j<s.length();j++){
+                
+                if(i==j); 
+                else if(s[i]==s[j]){
+                    break;
+                }
+            }
+         //   cout<<s[j]<<endl;
+            if(j==s.length()){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
