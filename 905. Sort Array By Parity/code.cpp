ffc2fa016c2971538b7eeddc4e37df004cbcbb30
Bharat Swami <79807425+bharat1718swami@users.noreply.github.com>
2023-07-18 17:46:33 +0530

    Easy, Two pointer, Array

---
+++ 905. Sort Array By Parity/code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+
+        int i=0;
+        int j=nums.size()-1;
+
+        while(i<j){
+            if(nums[i]%2==1){
+                swap(nums[i],nums[j]);
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return nums;
+        
+    }
+};
